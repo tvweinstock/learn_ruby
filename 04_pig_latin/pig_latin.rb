cefa97faedf35342ff4411b1c6936d46c38a6e39
Tobi Weinstock <tv.weinstock@gmail.com>
2014-08-26 09:07:29 -0400

    pig latin test passes

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -5,8 +5,8 @@ def translate(word)
   if vowels.include?(word[0])
     return word + "ay"
   elsif consonants.include?(word[0]) && consonants.include?(word[1])
-    return word.slice(1..2, word.size) + word[0] + "ay"
-    # return word[2..-1] + word[0..1] + 'ay'    
+    # return word.slice(1..2, word.size) + word[0] + "ay"
+    return word[2..-1] + word[0..1] + 'ay'    
   else
     return word.slice(1, word.size) + word[0] + "ay"
   end
